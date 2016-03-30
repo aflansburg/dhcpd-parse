e3a8558bf9664f62fc8f59fbbd5360eef67ef758
Abram Flansburg <abe.flansburg@gmail.com>
2016-03-30 08:41:14 -0500

    Command line arguments to accept file path.

--- parse_leases.py
+++ parse_leases.py
@@ -1,10 +1,14 @@
-## Had some help with the regex from some StackOverflow folks ##
+import os, sys, re
 
-# test comment #
+if len(sys.argv) == 2:
+    fp = str(sys.argv[1])
+elif len(sys.argv) > 2:
+    print 'This tool takes no more than one argument (not counting the .py script) which is the filename.' \
+          'Usage: python parse_leases.py /User/someuser/filepath.txt'
+else:
+    fp = raw_input("Please enter the file path: ")
+    fp = str(fp)
 
-import sys, re
-
-fp = '/Users/misaflansb/Dropbox/ERMCO/Network/DHCP-Info/dhcpd.leases'
 p = re.compile("lease ([0-9.]+) {.*?hardware ethernet ([:a-f0-9]+);.*?}", re.MULTILINE | re.DOTALL)
 
 ip_list = []
