692fb0702e93455840d5abfd4aa53f378fa5d6c4
Abram Flansburg <abe.flansburg@gmail.com>
2016-03-30 08:11:26 -0500

    Initial version

---
+++ parse_leases.py
@@ -0,0 +1,15 @@
+__author__ = 'misaflansb'
+
+import sys, re
+
+fp = '/Users/misaflansb/Dropbox/ERMCO/Network/DHCP-Info/dhcpd.leases'
+p = re.compile("lease ([0-9.]+) {.*?hardware ethernet ([:a-f0-9]+);.*?}", re.MULTILINE | re.DOTALL)
+
+ip_list = []
+
+with open(fp) as f:
+    for match in p.finditer(f.read()):
+        ip_list.append(match)
+        print("IP: {0}  MAC: {1}".format(match.group(1), match.group(2)))
+
+print "Total IP addresses in dhcpd.leases = {0}".format(len(ip_list))
